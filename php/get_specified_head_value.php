46ebd181924c840e47c1b004d79345d76f9f815f
lyroge@foxmail.com <lyroge@foxmail.com>
2013-05-20 17:33:31 +0800

    php directory

---
+++ php/get_specified_head_value.php
@@ -0,0 +1,6 @@
+<?php
+	echo $_SERVER['HTTP_REFERER'].'<br/>';
+	echo $_SERVER['HTTP_USER_AGENT'].'<br/>';
+	echo $_SERVER['REMOTE_ADDR'].'<br/>';
+	echo $_SERVER['HTTP_X_FORWARDED_FOR'].'<br/>';
+?>
