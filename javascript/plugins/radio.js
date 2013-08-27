9af8c631d98d9865b11e3174986a663fba380f54
lyroge <lyroge@foxmail.com>
2013-08-27 15:07:36 +0800

    radio.js

--- javascript/plugins/radio.js
+++ javascript/plugins/radio.js
@@ -3,7 +3,7 @@ function is_radio_checked(name){
 	var obj = document.getElementsByName(name);
 	for(i=0;i<obj.length;i++){
 		if(obj[i].checked){
-			b = true;
+			return true;
 		}
 	}
 	return false;
