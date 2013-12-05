564c95a5267e6424ad1af99b55f31b021e8b0dbd
lyroge <lyroge@foxmail.com>
2013-12-05 17:07:30 +0800

    cookie fixed

--- python/scrapyserp/main.py
+++ python/scrapyserp/main.py
@@ -1,16 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 
-url = 'http://www.google.com.hk/search?q=leg+warmers&safe=strict&hl=zh-HK&gbv=1&ei=rjCgUpfxPMSFiQeJwoHYDQ&start=%s&sa=N'
+url = 'http://www.google.com.hk/search?q=leg+warmers'
 #url = 'http://pgl.yoyo.org/http/browser-headers.php'
-
-
 
 from scrapyrobot import ProxyScrapy
 
 curl = ProxyScrapy()
 
-for i in [0, 10, 20]:
-	a=curl.get_html_body(url % str(i))
-	f=open('%s.txt' % str(i), 'w')
-	f.write(a)
+a=curl.get_html_body(url)
+f=open('1.html', 'w')
+f.write(a)
--- python/scrapyserp/scrapyrobot.py
+++ python/scrapyserp/scrapyrobot.py
@@ -13,11 +13,12 @@ class ProxyScrapy(object):
     def __init__(self):
         self.proxy_robot = ProxyRobot()
         self.current_proxy = None
+        self.cookie = cookielib.CookieJar()
     
     def __builder_proxy_cookie_opener(self):
         self.current_proxy = ip_port = self.proxy_robot.get_random_proxy()
-        cookie = cookielib.CookieJar()
-        cookie_handler = urllib2.HTTPCookieProcessor(cookie)
+        
+        cookie_handler = urllib2.HTTPCookieProcessor(self.cookie)
         proxy_handler = urllib2.ProxyHandler({'http': ip_port[7:]})
 
         opener = urllib2.build_opener(proxy_handler, cookie_handler)
@@ -38,6 +39,10 @@ class ProxyScrapy(object):
         request=urllib2.Request(url)
         request.add_header("User-Agent",random.choice(USER_AGENT_LIST))
         request.add_header("Accept-Charset", "GBK,utf-8;q=0.7,*;q=0.3")
+        request.add_header("Accept-Encoding", "gzip,deflate,sdch")
+        request.add_header("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
+        request.add_header("Cache-Control", "no-cache")
+        request.add_header("Connection", "keep-alive")
 
         try:
             response = opener.open(request)
