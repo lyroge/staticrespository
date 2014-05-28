98b7f20a702e4fee69eba6d731d57572d8357a21
lyroge <lyroge@foxmail.com>
2014-05-28 16:52:50 +0800

    中文编码问题

--- python/discuz/main.py
+++ python/discuz/main.py
@@ -5,7 +5,11 @@ from const.db import HOST,USER,PASSWD,DB
 import random, time, datetime
 
 conn = MySQLdb.connect(host=HOST,user=USER,passwd=PASSWD,db=DB,cursorclass=MySQLdb.cursors.DictCursor)
+conn.set_character_set('utf8')
 cursor=conn.cursor()
+cursor.execute('SET NAMES utf8;')
+cursor.execute('SET CHARACTER SET utf8;')
+cursor.execute('SET character_set_connection=utf8;')
 
 def timestamp(dtstr):
 	if not dtstr:
@@ -32,29 +36,14 @@ def post(subject, content, fid, authorid, author, posttime):
 	cursor.execute('INSERT INTO pre_forum_post (pid, fid, tid, author, authorid, subject, dateline, message, useip, port) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)', param)
 
 	#更新论坛版块内容 pre_forum_forum
-	lastpost = '%s    %s    %s    %s' % (tid, subject, unixtime, author)
+	lastpost = '%s	%s	%s	%s' % (tid, subject, unixtime, author)
 	cursor.execute('update pre_forum_forum set posts=posts+1, todayposts=todayposts+1, lastpost=%s where fid=%s', (lastpost,fid))
 
 	#更新用户统计数据 pre_common_member_count
-	lastpost = '%s    %s    %s    %s' % (tid, subject, unixtime, author)
 	cursor.execute('update pre_common_member_count set posts=posts+1, threads=threads+1 where uid=%s', (authorid,))
+	print 'done'
 
-	print '发帖完毕'
+post("测试帖子", "测试帖子内容", 69, 12, "资讯小编", "")
 
-post("hello", "discuz", 39, 2, "abc", "")
-
-'''
-cursor.execute(START_URL_SQL)
-result = cursor.fetchall()
-for r in result:
-	url = '<a href="http://www.freeb2bmarket.com/company/%s.html">%s</a>' % (r['url_slug'], r['name'])
-	person = r['contactperpon']
-	email = r['email']
-	title = '%s Product details required' % person
-	send_email(email, title, person, url)
-	print r['name']
-	time.sleep(random.choice([1,3,5,7]))
 cursor.close() 
-conn.close()'''
-
-
+conn.close()
