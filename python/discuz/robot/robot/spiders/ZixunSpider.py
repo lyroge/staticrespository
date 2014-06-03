01fe1e02f352fb58f6ab87b34e377548603c5d70
lyroge <lyroge@foxmail.com>
2014-06-03 16:15:58 +0800

    加入社会热点和房产新闻资料

--- python/discuz/robot/robot/spiders/ZixunSpider.py
+++ python/discuz/robot/robot/spiders/ZixunSpider.py
@@ -41,7 +41,7 @@ class ZixunSpider(CrawlSpider):
     name = "Zixun"
     allowed_domains = []
 
-    start_urls = ['http://www.echang.cn/house/index.asp','http://www.echang.cn/2shou/?page=1','http://www.echang.cn/job/qy.asp?gw=&lx=&sj=365&gz=&xl=&key=&page=1']
+    start_urls = ['http://www.echang.cn/house/index.asp','http://www.echang.cn/2shou/?page=1','http://www.echang.cn/job/qy.asp?gw=&lx=&sj=365&gz=&xl=&key=&page=1','http://news.163.com/','http://bj.house.163.com/']
 
     rules = (
         #易畅二手市场
@@ -52,7 +52,13 @@ class ZixunSpider(CrawlSpider):
         Rule(SgmlLinkExtractor(unique=True,allow=('/job/zhaopin.asp\?id=\d+$', )), callback='parse_echang_zhaopin'),
 
         #易畅求租
-        Rule(SgmlLinkExtractor(unique=True,allow=('/house/xiangxi.asp\?id=\d+$')), callback='parse_echang_fangchan')
+        Rule(SgmlLinkExtractor(unique=True,allow=('/house/xiangxi.asp\?id=\d+$')), callback='parse_echang_fangchan'),
+
+        #163新闻
+        Rule(SgmlLinkExtractor(unique=True,allow=('http://news.163.com/\d{1,2}/\d{1,4}/\d{1,2}/\w+.html$'),restrict_xpaths=('//div[@id="news"]/h2/a',)), callback='parse_163_news'),
+
+        #163房产
+        Rule(SgmlLinkExtractor(unique=True,allow=('http://bj.house.163.com/\d{1,2}/\d{1,4}/\d{1,2}/\w+.html$'),restrict_xpaths=('//div[@class="mod-list main-list news-date-list on"][position()=1]//a',)), callback='parse_163_house')
     ,)
 
     def __del__(self):
@@ -70,7 +76,7 @@ class ZixunSpider(CrawlSpider):
         #super init 
         super(ZixunSpider, self).__init__()
 
-    def post(self, subject, content, fid, authorid, author, posttime, typeid):
+    def post(self, subject, content, fid, authorid, author, posttime, typeid, htmlon=0, bbcodeoff=0):
         #插入主题
         unixtime = timestamp(posttime)  
         param = (fid, author, authorid, subject, unixtime, unixtime, author, typeid, random.randint(8, 51))
@@ -82,8 +88,8 @@ class ZixunSpider(CrawlSpider):
         pid = self.cursor.lastrowid
 
         #插入主题内容 pre_forum_post [htmlon=1, bbcodeoff=-1] 允许帖子中html代码
-        param = (pid, fid, tid, author, authorid, subject, unixtime, content, '127.0.0.1', '22622')
-        self.cursor.execute('INSERT INTO pre_forum_post (pid, fid, tid, author, authorid, subject, dateline, message, useip, port) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)', param)
+        param = (pid, fid, tid, author, authorid, subject, unixtime, content, '127.0.0.1', '22622', htmlon, bbcodeoff)
+        self.cursor.execute('INSERT INTO pre_forum_post (pid, fid, tid, author, authorid, subject, dateline, message, useip, port, htmlon, bbcodeoff) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)', param)
 
         #更新论坛版块内容 pre_forum_forum
         lastpost = '%s	%s	%s	%s' % (tid, subject, unixtime, author)
@@ -91,6 +97,75 @@ class ZixunSpider(CrawlSpider):
 
         #更新用户统计数据 pre_common_member_count
         self.cursor.execute('update pre_common_member_count set posts=posts+1, threads=threads+1 where uid=%s', (authorid,))
+
+    #网易新闻
+    def parse_163_news(self, response):
+        hxs = HtmlXPathSelector(response)
+        url = response.url
+        urlmd5 = md5(url)
+        self.cursor.execute('select url from url_history where urlmd5=%s', (urlmd5,))
+        r = self.cursor.fetchone()
+        if  r:
+            print 'scraped'
+            return None
+
+        #设置用户、版块、类别等信息 uid:12 fid:41
+        uid = 12
+        fid = 71
+        typeid = 0
+
+        title = ''.join(hxs.select(u'//h1[@id="h1title"]/text()').extract()).encode('utf8')
+        content = '<br/>'.join(hxs.select(u'//div[@id="endText"]/p').extract()).encode('utf8')
+
+        if title:
+            print '163news'
+
+            author =  '资讯小编'
+            d1 = datetime.datetime.now(tz)
+            #d3 = d1 + datetime.timedelta(days = random.randint(-20, 0))
+            d3 = d1 + datetime.timedelta(hours = random.randint(-1, 0))
+            d3 = d3 + datetime.timedelta(minutes = random.randint(-30, 12))
+            d3 = d3 + datetime.timedelta(seconds = random.randint(-45, 2))
+            posttime = d3.strftime('%Y-%m-%d %H:%M')
+            self.post(title, content, fid, uid, author, posttime, typeid, 1, -1)
+
+            #记录痕迹
+            self.cursor.execute('insert url_history(url, urlmd5) values(%s, %s)', (url, urlmd5))
+
+    #网易房产
+    def parse_163_house(self, response):
+        hxs = HtmlXPathSelector(response)
+        url = response.url
+        urlmd5 = md5(url)
+        self.cursor.execute('select url from url_history where urlmd5=%s', (urlmd5,))
+        r = self.cursor.fetchone()
+        if  r:
+            print 'scraped'
+            return None
+
+        #设置用户、版块、类别等信息 uid:12 fid:41
+        uid = 12
+        fid = 38
+        typeid = 0
+
+        title = ''.join(hxs.select(u'//h1[@id="h1title"]/text()').extract()).encode('utf8')
+        content = '<br/>'.join(hxs.select(u'//div[@id="endText"]/p').extract()).encode('utf8')
+
+        if title:
+            print '163news'
+
+            author =  '资讯小编'
+            d1 = datetime.datetime.now(tz)
+            #d3 = d1 + datetime.timedelta(days = random.randint(-20, 0))
+            d3 = d1 + datetime.timedelta(hours = random.randint(-1, 0))
+            d3 = d3 + datetime.timedelta(minutes = random.randint(-30, 12))
+            d3 = d3 + datetime.timedelta(seconds = random.randint(-45, 2))
+            posttime = d3.strftime('%Y-%m-%d %H:%M')
+            self.post(title, content, fid, uid, author, posttime, typeid, 1, -1)
+
+            #记录痕迹
+            self.cursor.execute('insert url_history(url, urlmd5) values(%s, %s)', (url, urlmd5))
+
 
     #易畅二手
     def parse_echang_ershou(self, response):
